ef4581baf305a88faad6d90d6e81540063da44dd
huyitus <huyitus@yahoo.com>
2023-06-23 14:09:36 +0700

    3 views

---
+++ LETAN/BAL/BALExample.cs
@@ -0,0 +1,15 @@
+﻿using QLKS.DAL;
+using System.Collections.Generic;
+
+namespace QLKS.BAL
+{
+    class BALExample
+    {
+        public static List<DALExample> getExamples()
+        {
+            return new List<DALExample>();
+        }
+
+        // ...
+    }
+}
