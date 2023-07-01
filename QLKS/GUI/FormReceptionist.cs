5d022654689c1b7617ad0f2d4b61c42a558ecee6
Nguyễn Thị Hồng Phúc <83939478+nthihongphuc@users.noreply.github.com>
2023-07-01 23:25:15 +0700

    update

---
+++ QLKS/GUI/FormReceptionist.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLKS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTracuuKH_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTracuuPhong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTracuuDV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTracuuTourDL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQLPhong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHoaDon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuiDinh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYCDaiLy_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
